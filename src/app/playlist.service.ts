b3c137c1cba9a6079fe54f172ca989bd47ec5550
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2023-04-07 23:18:30 +0200

    Updated Services

--- src/app/playlist.service.ts
+++ src/app/playlist.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
+import { Song } from 'src/app/models/Song';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  createPlaylist(body: String) {
+    return this.http.post('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/playlists', body, httpOptions);
+  }
+
+  addSong(id: number, body: String) {
+    return this.http.patch('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/playlists/' + id, body, httpOptions);
+  }
+
+  delSong(id: number, body: String) {
+    return this.http.patch('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/playlists/' + id, body, httpOptions);
+  }
+
+  getPlaylistAt(id: number) {
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/playlists/' + id);
+  }
 }
