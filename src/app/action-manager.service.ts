d8548d90598fc446260e6e0605c48e60ba9637bd
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2023-04-07 23:18:30 +0200

    Updated Services

--- src/app/action-manager.service.ts
+++ src/app/action-manager.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Song } from 'src/app/models/Song';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionManagerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getArtist() {
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/artists');
+
+  }
+
+  getArtistAt(id: number) {
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/artists'+id);
+  }
+
+  getAlbum() {
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/albums');
+  }
+
+  getAlbumAt(id: number) {
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/albums'+id);
+  }
 }
