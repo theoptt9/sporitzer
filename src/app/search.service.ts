6ab23beb719e7c159d5438a75284a3026d94f31a
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2023-04-07 21:22:00 +0200

    add models

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Song } from 'src/app/models/Song';
 
 @Injectable({
@@ -13,34 +13,34 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   async searchWith(_criteria: String) {
-    
+
   }
 
   private async _initialize() {
     this.songs = await this.retrieveSongs();
     this.initialized = true;
-}
+  }
 
-  async retrieveSongs() : Promise<Array<Song>> {
+  async retrieveSongs(): Promise<Array<Song>> {
     return new Promise((resolve, reject) => {
       this.retrieveAllSongs().subscribe(
-          (songs: Array<string>) => {
-              let promises: Array<Promise<any>> = [];
-              songs.forEach((value, index, array) => {
-                  promises.push(this.retrieveOneSong(value).toPromise());
-              })
-              Promise.all(promises).then(infos => {
-                  console.log(infos);
-                  resolve(infos);
-              }).catch(reason => {
-                  reject(reason);
-              })
-          },
+        (songs: Array<string>) => {
+          let promises: Array<Promise<any>> = [];
+          songs.forEach((value, index, array) => {
+            promises.push(this.retrieveOneSong(value).toPromise());
+          })
+          Promise.all(promises).then(infos => {
+            console.log(infos);
+            resolve(infos);
+          }).catch(reason => {
+            reject(reason);
+          })
+        },
         (error1: any) => {
-              reject(error1);
-          }
+          reject(error1);
+        }
       )
-  });
+    });
   }
 
   async retrieveAllSongs() {
@@ -48,19 +48,19 @@ export class SearchService {
   }
 
   private retrieveOneSong(id: any) {
-    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/songs/'+id);
-}
+    return this.http.get('https://mmi.unilim.fr/~morap01/L250/public/index.php/api/songs/' + id);
+  }
 
-/**
-     * Retourne l'index dans le tableau de chanson de l'élément
-     * ayant l'id passé en paramètre
-     * @param id
-     * @private
-     */
-private _getModelIndexById(id: number) {
-  return this.songs.findIndex(function (element) {
-  return element.id === id;
-  });
-}
+  /**
+       * Retourne l'index dans le tableau de chanson de l'élément
+       * ayant l'id passé en paramètre
+       * @param id
+       * @private
+       */
+  private _getModelIndexById(id: number) {
+    return this.songs.findIndex(function (element) {
+      return element.id === id;
+    });
+  }
 
 }
