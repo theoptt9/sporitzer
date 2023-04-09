3265ccc34ad956c35cdd9bf610b1bcc1b7089f67
theoptt9 <83294701+theoptt9@users.noreply.github.com>
2023-04-09 12:30:23 +0200

    Style branch (#5)
    * Add library detail
    
    * Add library detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { LibraryComponent } from './pages/library/library.component';
+import { LibraryDetailComponent } from './pages/library-detail/library-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'search', component: SearchComponent},
   { path: 'library', component: LibraryComponent},
+  { path: 'playlist/:name', component: LibraryDetailComponent },
 ];
 
 @NgModule({
